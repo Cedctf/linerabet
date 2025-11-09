c394af496b41ce1887f33582ce440e5fc1e1a036
Cedric <cedricctf11a@gmail.com>
2025-11-10 01:31:05 +0800

    setup linera smart contract folder

---
+++ smartcontract/src/lib.rs
@@ -0,0 +1,23 @@
+use async_graphql::{Request, Response};
+use linera_sdk::{
+    graphql::GraphQLMutationRoot,
+    linera_base_types::{ContractAbi, ServiceAbi},
+};
+use serde::{Deserialize, Serialize};
+
+pub struct SmartcontractAbi;
+
+impl ContractAbi for SmartcontractAbi {
+    type Operation = Operation;
+    type Response = ();
+}
+
+impl ServiceAbi for SmartcontractAbi {
+    type Query = Request;
+    type QueryResponse = Response;
+}
+
+#[derive(Debug, Deserialize, Serialize, GraphQLMutationRoot)]
+pub enum Operation {
+    Increment { value: u64 },
+}
