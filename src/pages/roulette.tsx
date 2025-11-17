e45c2a4cb7cbfaf54055e2ee0899ef4d01a07bee
Cedric <cedricctf11a@gmail.com>
2025-11-17 19:13:26 +0800

    fixed build errors

--- src/pages/roulette.tsx
+++ src/pages/roulette.tsx
@@ -11,14 +11,6 @@ import {
   type SpinResult,
 } from '../lib/roulette-chain';
 
-// Roulette wheel numbers in order (American roulette with 00)
-const WHEEL_ORDER = [
-  0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1, '00',
-  27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2
-];
-
-// Formatted bet options for RouletteWheel component
-const BET_OPTIONS = WHEEL_ORDER.map(num => num.toString());
 
 // Red numbers in roulette (American roulette standard)
 const RED_NUMBERS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 28, 30, 32, 34, 36];
@@ -225,7 +217,6 @@ export default function Roulette() {
                   start={startWheel}
                   winningBet={winningBet}
                   onSpinningEnd={handleSpinningEnd}
-                  betOptions={BET_OPTIONS}
                 />
                 
                 {/* Result display overlay */}
