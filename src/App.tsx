8d27b282ab25006495e7f09bd83186d40929afef
derek2403 <derekliew0@gmail.com>
2026-01-10 20:08:31 +0800

    remove poker cards done blackjack 2 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, Blackjack, Baccarat, Roulette, Games } from './pages';
+import { Home, Blackjack, Blackjack2, Baccarat, Roulette, Games } from './pages';
 import Layout from './components/Layout';
 import "./App.css";
 
@@ -14,6 +14,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/games" element={<Games />} />
             <Route path="/blackjack" element={<Blackjack />} />
+            <Route path="/blackjack2" element={<Blackjack2 />} />
             <Route path="/baccarat" element={<Baccarat />} />
             <Route path="/roulette" element={<Roulette />} />
           </Route>
